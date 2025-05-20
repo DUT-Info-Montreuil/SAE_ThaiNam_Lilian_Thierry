e5a890bc969a6aefd758f90c5946e6004fb4fa25
Danton <liliandanton2@gmail.com>
2025-05-20 11:06:41 +0200

    modifier l'inventaire + le agit du joueur + creer epee/arc +modif dans pressedkey

---
+++ src/main/java/universite_paris8/iut/tngomarie_tchen_dlillian/sae/modele/Objet/Arc.java
@@ -0,0 +1,8 @@
+package universite_paris8.iut.tngomarie_tchen_dlillian.sae.modele.Objet;
+
+public class Arc extends Objet{
+
+    public Arc(int id){
+        super(id);
+    }
+}
---
+++ src/main/java/universite_paris8/iut/tngomarie_tchen_dlillian/sae/modele/Objet/Inventaire.java
@@ -0,0 +1,30 @@
+package universite_paris8.iut.tngomarie_tchen_dlillian.sae.modele.Objet;
+
+import java.util.ArrayList;
+
+public class Inventaire {
+    private ArrayList<Objet> Inventaire;
+    private int enMain;
+    private int caseVide = 0;
+
+    public Inventaire(){
+        this.Inventaire = new ArrayList<Objet>();
+        this.enMain = 0;
+    }
+
+    public Objet objetEnMain(){
+        return Inventaire.get(enMain);
+    }
+
+    public void changerObjet(int num){
+        this.enMain = num;
+    }
+    public int getEnMain(){
+        return this.enMain;
+    }
+
+    public void ajoutObjet(Objet objet){
+        this.Inventaire.add(caseVide,objet);
+        caseVide++;
+    }
+}
