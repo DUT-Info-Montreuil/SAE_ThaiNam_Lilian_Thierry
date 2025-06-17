f25f0b8794374ab8a8fdb46445a382d7fdec62ec
thai-nam <tn.ngo.marie8@gmail.com>
2025-06-17 10:59:24 +0200

    fixed inversion of heigt and width

---
+++ src/main/java/universite_paris8/iut/tngomarie_tchen_dlillian/sae/Param.java
@@ -0,0 +1,7 @@
+package universite_paris8.iut.tngomarie_tchen_dlillian.sae;
+
+public class Param {
+    public static int width =256;
+    public static int height=64;
+    public static int scale=16;
+}
