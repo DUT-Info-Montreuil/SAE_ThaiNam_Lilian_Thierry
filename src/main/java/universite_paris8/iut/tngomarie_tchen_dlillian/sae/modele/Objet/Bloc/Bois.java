256d1b4699e658ac14eccba205531d2161c0a644
tngomarie <tn.ngo.marie8@gmail.com>
2025-09-23 16:46:59 +0200

    rendu finale

--- src/main/java/universite_paris8/iut/tngomarie_tchen_dlillian/sae/modele/Objet/Bloc/Bois.java
+++ src/main/java/universite_paris8/iut/tngomarie_tchen_dlillian/sae/modele/Objet/Bloc/Bois.java
@@ -7,7 +7,7 @@ import universite_paris8.iut.tngomarie_tchen_dlillian.sae.modele.Objet.Objet;
 
 public class Bois extends BlocInv   {
     public Bois(int nb) {
-        super(0, nb);
+        super(2, nb);
     }
 
     @Override
--- src/main/java/universite_paris8/iut/tngomarie_tchen_dlillian/sae/modele/Objet/Bloc/Pierre.java
+++ src/main/java/universite_paris8/iut/tngomarie_tchen_dlillian/sae/modele/Objet/Bloc/Pierre.java
@@ -11,16 +11,6 @@ public class Pierre extends BlocInv {
     }
 
     @Override
-    public void agit(Player player,double SourisX,double SourisY) {
-            int x=Math.toIntExact(Math.round(SourisX/ Param.scale));
-            int y=Math.toIntExact(Math.round(SourisY/ Param.scale));
-            if(player.getEnv().getMap1()[y][x]==0) {
-                player.getEnv().changeBlock(y, x, this.getIdObjet());
-                player.getVueTerrain().changementTuileMinage(x, y, this.getIdObjet());
-            }
-    }
-
-    @Override
     public ImageView getimage() {
         Image image = new Image("Texture de pierre.png");
         ImageView imageView = new ImageView(image);
